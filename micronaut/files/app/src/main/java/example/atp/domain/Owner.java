2dff4ac2eb274377375b6079d2fff2a045acc3d0
Chris <cbensen@gmail.com>
2020-10-15 08:56:33 -0700

    change

--- micronaut/files/app/src/main/java/example/atp/domain/Owner.java
+++ micronaut/files/app/src/main/java/example/atp/domain/Owner.java
@@ -4,6 +4,7 @@ import io.micronaut.core.annotation.Creator;
 import io.micronaut.data.annotation.GeneratedValue;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.GeneratedValue;
 
 @MappedEntity
 public class Owner {
