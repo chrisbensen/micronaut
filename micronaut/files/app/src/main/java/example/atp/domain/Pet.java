25b879048782c5e0c58f53fca974233bd1d39fff
Chris <cbensen@gmail.com>
2020-10-15 08:11:40 -0700

    changes to schema  remove sequence

--- micronaut/files/app/src/main/java/example/atp/domain/Pet.java
+++ micronaut/files/app/src/main/java/example/atp/domain/Pet.java
@@ -5,6 +5,7 @@ import io.micronaut.data.annotation.AutoPopulated;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 import io.micronaut.data.annotation.Relation;
+import io.micronaut.data.annotation.GeneratedValue;
 
 import javax.annotation.Nullable;
 import java.util.UUID;
@@ -13,11 +14,10 @@ import java.util.UUID;
 public class Pet {
 
     @Id
-    @AutoPopulated
-    private UUID id;
+    @GeneratedValue(Type.IDENTITY)
+    private Long id;
     private String name;
-    //@Relation(Relation.Kind.MANY_TO_ONE)
-    @Relation(GeneratedValue.Type)
+    @Relation(Relation.Kind.MANY_TO_ONE)
     private Owner owner;
     private PetType type = PetType.DOG;
 
